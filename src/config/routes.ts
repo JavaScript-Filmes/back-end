b584c1d71fd906731f8467f2f316ba0c18f8f889
Thaina Biudes <thaina.biudes@gmail.com>
2021-04-26 22:41:44 -0300

    feat: cria rotas

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { FilmeController } from "../controllers/FilmeController";
 import { LocacaoController } from "../controllers/LocacaoController";
+import { ClienteController } from "../controllers/ClienteController";
+import { FavoritoController } from "../controllers/FavoritoController";
 
 const router = Router();
 const filmeController = new FilmeController();
 const locacaoController = new LocacaoController();
+const clienteController = new ClienteController();
+const favoritoController = new FavoritoController();
 
 // Rotas endpoint de filmes
 router.get("/", filmeController.listar);
@@ -19,5 +23,13 @@ router.get("/catalogo/listar-locacao/:id", locacaoController.listarPorId);
 router.post("/catalogo/cadastrar-locacao", locacaoController.cadastrar);
 router.delete("/catalogo/excluir-locacao", filmeController.excluir);
 router.put("/catalogo/editar-locacao", locacaoController.editar);
+
+// Rotas endpoint de usuário
+router.get("/catalogo/cliente", clienteController.listar);
+router.post("/catalogo/cadastrar-usuario", clienteController.cadastrar);
+
+// Rotas endpoint de favoritos
+router.post("/catalogo/cadastrar-favorito", favoritoController.cadastrar);
+router.delete("/catalogo/excluir-favorito", favoritoController.excluir);
 
 export { router };
