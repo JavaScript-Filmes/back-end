29328c290b351168c8d80ca029f7e93c31cffdd7
Thaina Biudes <thaina.biudes@gmail.com>
2021-04-26 22:41:21 -0300

    feat: cria models

---
+++ src/models/FavoritoSchema.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+
+const favoritoSchema = new Schema(
+  {
+    cliente: [{ type: Schema.Types.ObjectId, ref: "Cliente" }],
+    filme: [{ type: Schema.Types.ObjectId, ref: "Filme" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Favorito", favoritoSchema);
