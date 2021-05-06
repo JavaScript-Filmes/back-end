e57318b30645c09451d3c380eeab9e1bbaedaa4a
Thaina Biudes <thaina.biudes@gmail.com>
2021-05-06 19:10:36 -0300

    feat: adiciona cliente se não tiver repetição na base

--- src/controllers/ClienteController.ts
+++ src/controllers/ClienteController.ts
@@ -19,12 +19,25 @@ class ClienteController {
 
   async cadastrar(request: Request, response: Response) {
     try {
-      const catalogo = await clienteSchema.create(request.body);
-      response.status(201).json({
-        data: catalogo,
-        error: false,
-        msg: "Cadastro efetuado com sucesso",
-      });
+      const list = await clienteSchema.find();
+
+      const isListed = list.find(
+        (data: any) => data.usuario === request.body.usuario
+      );
+
+      if (!isListed) {
+        const result = await clienteSchema.create(request.body);
+        response.status(201).json({
+          data: result,
+          error: false,
+          msg: "Cadastro efetuado com sucesso",
+        });
+      } else {
+        response.status(409).json({
+          error: true,
+          msg: "Erro! Este usuário já está em nossa base de dados.",
+        });
+      }
     } catch (error) {
       response.status(400).json({
         data: error.message,
